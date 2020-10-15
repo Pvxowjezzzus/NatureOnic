8cffb85ca2cf9bc70657574d57e5f47916f4271a
Pvxow da Jezzzus <pasha.klimanov95@gmail.com>
2020-10-15 23:14:14 +0300

    Резервная версия ресурса

--- public_html/resource/js/script.js
+++
@@ -1,9 +0,0 @@
-var menu = document.getElementById('adaptive-menu');
-menu.onclick = function() {
-    menu.classList.toggle('active');
-}
-window.onclick = function(e) {
-    if (!menu.contains(e.target)) {
-        menu.classList.remove('active');
-    }
-}
